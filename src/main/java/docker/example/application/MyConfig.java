04417294acffae71c05d63fffcff024102a20c60
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 02:09:40 +0300

    init

--- src/main/java/docker/example/application/MyConfig.java
+++ src/main/java/docker/example/application/MyConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
@@ -11,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class MyConfig {
 
 }
