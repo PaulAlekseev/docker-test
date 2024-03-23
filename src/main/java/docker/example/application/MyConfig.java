a18b9a50310d794202453188963ae351ca9e21a4
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 01:28:55 +0300

    init

--- src/main/java/docker/example/application/MyConfig.java
+++ src/main/java/docker/example/application/MyConfig.java
@@ -1,8 +1,10 @@
 package docker.example.application;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
@@ -16,5 +18,14 @@ public class MyConfig {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return TopicBuilder.name("thing1")
+                .partitions(10)
+                .replicas(3)
+                .compact()
+                .build();
     }
 }
