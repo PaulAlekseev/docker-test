16943899c31b7402596df3d30a616e2e34fdb3f5
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 02:11:57 +0300

    init

--- src/main/java/docker/example/application/MyConfig.java
+++ src/main/java/docker/example/application/MyConfig.java
@@ -1,18 +1,21 @@
 package docker.example.application;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class MyConfig {
 
+    @Bean
+    public NewTopic topic1() {
+        return TopicBuilder.name("thing1")
+                .partitions(10)
+                .replicas(3)
+                .compact()
+                .build();
+    }
 }
