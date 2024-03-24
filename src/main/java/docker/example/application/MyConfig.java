5160d2b1eb91ecf04a53b9328d0994c03a294f89
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 03:08:44 +0300

    init

--- src/main/java/docker/example/application/MyConfig.java
+++ src/main/java/docker/example/application/MyConfig.java
@@ -1,19 +1,16 @@
 package docker.example.application;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import docker.example.application.dto.AbstractDto;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +18,13 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class MyConfig {
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     @Bean
     public ProducerFactory<String, AbstractDto> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
