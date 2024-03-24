88354370cce396ed4e6941eced5f1d20e2d1474b
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 17:01:24 +0300

    init

--- src/main/java/ru/maiklk/microtwo/controller/ProducerController.java
+++ src/main/java/ru/maiklk/microtwo/controller/ProducerController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class ProducerController {
     public ResponseEntity<String> sendMessage(@RequestBody MessageDto dto) {
         kafkaService.send(dto);
         return new ResponseEntity<>(String.format("Successfully autowired: %s", kafkaServerHost), HttpStatus.OK);
+    }
+
+    @GetMapping("/cum")
+    public ResponseEntity<String> sendMessage1() {
+        return new ResponseEntity<>(String.format(kafkaServerHost), HttpStatus.OK);
     }
 }
