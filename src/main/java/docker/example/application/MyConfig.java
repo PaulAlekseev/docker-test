d8201fd55d1ea9d17c48eb181593dae0ed6585db
pavel <pavel.zommerfeld@yandex.ru>
2024-03-24 04:16:30 +0300

    init

--- src/main/java/docker/example/application/MyConfig.java
+++ src/main/java/docker/example/application/MyConfig.java
@@ -2,11 +2,17 @@ package docker.example.application;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,12 +21,24 @@ import java.util.Map;
 @EnableKafka
 public class MyConfig {
     @Bean
-    public KafkaAdmin admin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        return new KafkaAdmin(configs);
+    public ProducerFactory<Integer, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        // See https://kafka.apache.org/documentation/#producerconfigs for more properties
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate() {
+        return new KafkaTemplate<Integer, String>(producerFactory());
+    }
     @Bean
     public NewTopic topic1() {
         return TopicBuilder.name("thing1")
--- src/main/java/docker/example/application/MyController.java
+++ src/main/java/docker/example/application/MyController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 @RequestMapping("/server/test")
@@ -12,6 +14,7 @@ import java.util.List;
 public class MyController {
 
     private final MyEntityRepository myEntityRepository;
+    private final KafkaService kafkaService;
 
     @GetMapping("/")
     public ResponseEntity<List<MyEntityDto>> getAllMyEntities() {
@@ -19,7 +22,8 @@ public class MyController {
     }
 
     @GetMapping("/fortnite")
-    public ResponseEntity<String> getHello() {
+    public ResponseEntity<String> getHello() throws ExecutionException, InterruptedException, TimeoutException {
+        kafkaService.sendToKafka();
         return ResponseEntity.ok().body("hello2");
     }
 
